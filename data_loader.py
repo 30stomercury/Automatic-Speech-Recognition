12139be7ba1c0a132eef4197fa5157026624e877
30stomercury <ff936tw@gmail.com>
2019-11-27 15:35:43 +0800

    add feature length on output batch

--- data_loader.py
+++ data_loader.py
@@ -31,13 +31,35 @@ wav_lens = tf.reduce_max(
   axis=-1) + 1 
 print(sess.run(wav_lens))
 
-def load_audio(path, sess, prepro_batch=128, frame_step=10, sample_rate=22050, feat_type='fbank'):
-    
+def load_audio(path, 
+               sess, 
+               prepro_batch=128, 
+               sample_rate=22050,
+               frame_step=10, 
+               feat_dim = 40,
+               feat_type='fbank'):
+    """GPU accerated audio features extracting in tensorflow
+
+    Args:
+        path: Path of libriSpeech.
+        sess: Tf session to execute the graph for feature extraction.
+        prepro_batch: Batch size for preprocessing audio features.
+        frame_step: Step size in ms.
+        feat_dim: Feature dimension.
+        feat_type: Types of features you want to apply.
+
+    Returns:
+        feats_list: List of features with variable length L, 
+                    each element is in the shape of (L, feat_dim), N is
+                    the number of samples.
+        length_list: List of feature length.
+    """    
+
+    # build extacting graph
     input_audio = tf.placeholder(dtype=tf.float32, shape=[None, None])
-    # extract audio feature:
     if feat_type == 'fbank':
         mel_fbanks = common_audio.compute_mel_filterbank_features(
-            input_audio, dither=0.0, sample_rate=sample_rate, frame_step=frame_step, num_mel_bins=40, apply_mask=True)
+            input_audio, sample_rate=sample_rate, frame_step=frame_step, num_mel_bins=feat_dim, apply_mask=True)
         mel_fbanks = tf.reduce_sum(mel_fbanks, -1)
 
     def extract_feat(audio_batch, len_batch, fs):
@@ -46,7 +68,7 @@ def load_audio(path, sess, prepro_batch=128, frame_step=10, sample_rate=22050, f
         for i in range(len(audio_batch)):
             audio_padded[i][:len(audio_batch[i])] = audio_batch[i]
         feat = sess.run(mel_fbanks, feed_dict={input_audio: audio_padded})
-        # find the feature length:
+        # compute the feature length:
         feat_len = np.array(len_batch) // int(fs * frame_step / 1e3) + 1
         feat_len = feat_len.astype(np.int32)
         return feat, feat_len
@@ -56,9 +78,10 @@ def load_audio(path, sess, prepro_batch=128, frame_step=10, sample_rate=22050, f
     feats_list = []
     length_list = []
 
+    # start extracting audio feature in a batch manner:
     dir_path = Path(path)
     audio_list = [f for f in dir_path.glob('**/*.flac') if f.is_file()] 
-    for p in audio_list:
+    for p in audio_list[:50]:
         audio, fs = librosa.load(p)
         audio_batch.append(audio)
         len_batch.append(len(audio))
@@ -66,7 +89,6 @@ def load_audio(path, sess, prepro_batch=128, frame_step=10, sample_rate=22050, f
             feat, feat_len = extract_feat(audio_batch, len_batch, fs)
             # remove paddings of audios batch:
             for index, l in enumerate(feat_len):
-                #feats.append(mel_fbanks[index][:l])
                 feats_list.append(feat[index][:l])
             length_list = np.concatenate([length_list, feat_len])
             audio_batch = []
@@ -75,43 +97,49 @@ def load_audio(path, sess, prepro_batch=128, frame_step=10, sample_rate=22050, f
 
     if len(audio_batch) % prepro_batch != 0:
         feat, feat_len = extract_feat(audio_batch, len_batch, fs)
-        # remove paddings of rest audios:
+        # remove paddings:
         for index, l in enumerate(feat_len):
-            #feats.append(mel_fbanks[index][:l])
             feats_list.append(feat[index][:l])
         length_list = np.concatenate([length_list, feat_len])
+        print("Processed samples: {}/{}".format(len(feats_list), len(audio_list)))
+
     return feats_list, length_list.astype(np.int32)
 
 def batch_gen(feats_list, length_list, batch_size=5):
 
+    """
+    Returns:
+        
+    """
     assert len(feats_list) == len(length_list)
-
+    num_batches = len(feats_list) // batch_size + int(len(feats_list) % batch_size != 0)
     def generator():
         buff = []
         for i, x in enumerate(feats_list):
             if i % batch_size == 0:
-                max_len = max(length_list[i:i+batch_size])
+                len_batch = length_list[i:i+batch_size]
+                max_len = max(len_batch)
             
             if i % batch_size == 0 and buff:
-                yield np.stack(buff, 0)
+                yield np.stack(buff, 0), len_batch
                 buff = []
             padded = np.zeros([max_len - x.shape[0], x.shape[1]], dtype=np.float32)
             x_padded = np.concatenate([x, padded], 0)
             buff.append(x_padded)
 
         if buff:
-            yield np.stack(buff, 0)
+            yield np.stack(buff, 0), len_batch
 
-    shapes = ([None, None, None])
+    shapes = ([None, None, None], [None])
+    types = (tf.float32, tf.int32)
     dataset = tf.data.Dataset.from_generator(generator,
-                                             output_types=tf.float32, 
+                                             output_types=types, 
                                              output_shapes=shapes)
     dataset.batch(batch_size).prefetch(1)
     dataset = dataset.shuffle(128*batch_size)
     dataset = dataset.repeat() 
-    iter_ = dataset.make_initializable_iterator()
-    x = iter_.get_next()
-    return iter_, x, dataset
+    iter = dataset.make_initializable_iterator()
+    return iter, num_batches
 
 
 #X = load_audio(audio_list[:500], prepro_batch=20)
@@ -123,14 +151,12 @@ def batch_gen(feats_list, length_list, batch_size=5):
 if __name__ == "__main__":
     s = time.time()
     libri_path = './data/LibriSpeech/dev-clean'
-    X, X_len = load_audio(libri_path, sess, prepro_batch=32)
-    print(X[0].shape, X[1].shape)
-    #X_out = sess.run(X)
-    iter_, x, dataset = batch_gen(X, X_len)
-    print("output_shape", dataset.output_shapes)
+    X, X_len = load_audio(libri_path, sess, prepro_batch=64)
+    iter_, num_batches = batch_gen(X, X_len, batch_size=32)
+    x = iter_.get_next()
     sess.run(tf.global_variables_initializer())
     sess.run(iter_.initializer)
-    print(sess.run(x).shape)
+    print(num_batches)
     for _ in range(100):
-        print(sess.run(x).shape)
+        print(sess.run(x)[0].shape, max(sess.run(x)[1]), sess.run(x)[1].shape)
     print(time.time()-s)
