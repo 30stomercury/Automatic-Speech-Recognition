1d5625750ae4c688eae7fe3fad8e4597b0e9b819
30stomercury <ff936tw@gmail.com>
2019-11-29 15:49:34 +0800

    add char preprocessing

--- data_loader.py
+++ data_loader.py
@@ -3,8 +3,7 @@ import tensorflow as tf
 import librosa
 import numpy as np
 from glob import glob
-import re
-import time
+import string
 
 def data_preparation(libri_path):
     """Prepare texts and its corresponding audio file path
@@ -26,10 +25,10 @@ def data_preparation(libri_path):
         for line in f.readlines():
             line_ = line.split(" ")
             audio_path.append(path+"/"+line_[0]+".flac")
-            texts.append(line[len(line_[0])+1:])
+            texts.append(line[len(line_[0])+1:-1])
     return texts, audio_path
 
-def load_audio(audio_path, 
+def process_audio(audio_path, 
                sess, 
                prepro_batch=128, 
                sample_rate=22050,
@@ -39,7 +38,7 @@ def load_audio(audio_path,
     """GPU accerated audio features extracting in tensorflow
 
     Args:
-        audio_path: Path audio files.
+        audio_path: List of path of audio files.
         sess: Tf session to execute the graph for feature extraction.
         prepro_batch: Batch size for preprocessing audio features.
         frame_step: Step size in ms.
@@ -47,10 +46,10 @@ def load_audio(audio_path,
         feat_type: Types of features you want to apply.
 
     Returns:
-        feats_list: List of features with variable length L, 
-                    each element is in the shape of (L, feat_dim), N is
-                    the number of samples.
-        length_list1: List of feature length.
+        feats: List of features with variable length L, 
+               each element is in the shape of (L, feat_dim), N is
+               the number of samples.
+        featlen: List of feature length.
     """    
 
     # build extacting graph
@@ -73,11 +72,11 @@ def load_audio(audio_path,
         
     audio_batch = []
     len_batch = []
-    feats_list = []
-    length_list = []
+    feats = []
+    featlen = []
 
     # start extracting audio feature in a batch manner:
-    for p in audio_path:
+    for p in audio_path[:50]:
         audio, fs = librosa.load(p)
         audio_batch.append(audio)
         len_batch.append(len(audio))
@@ -85,42 +84,83 @@ def load_audio(audio_path,
             feat, feat_len = extract_feat(audio_batch, len_batch, fs)
             # remove paddings of audios batch:
             for index, l in enumerate(feat_len):
-                feats_list.append(feat[index][:l])
-            length_list = np.concatenate([length_list, feat_len])
+                feats.append(feat[index][:l])
+            featlen = np.concatenate([featlen, feat_len])
             audio_batch = []
             len_batch = []
-            print("Processed samples: {}/{}".format(len(feats_list), len(audio_path)))
+            print("Processed samples: {}/{}".format(len(feats), len(audio_path)))
 
     if len(audio_batch) % prepro_batch != 0:
         feat, feat_len = extract_feat(audio_batch, len_batch, fs)
         # remove paddings:
         for index, l in enumerate(feat_len):
-            feats_list.append(feat[index][:l])
-        length_list = np.concatenate([length_list, feat_len])
-        print("Processed samples: {}/{}".format(len(feats_list), len(audio_path)))
+            feats.append(feat[index][:l])
+        featlen = np.concatenate([featlen, feat_len])
+        print("Processed samples: {}/{}".format(len(feats), len(audio_path)))
 
-    return feats_list, length_list.astype(np.int32)
+    return np.array(feats), featlen.astype(np.int32)
 
-
-def batch_gen(feats_list, length_list1, batch_size=5):
-
+def process_texts(special_chars, texts):
     """
     Returns:
-        batches        
-
+        chars: List of index sequences.
+        charlen: List of length of sequences.
     """
-    assert len(feats_list) == len(length_list1)
-    num_batches = len(feats_list) // batch_size + int(len(feats_list) % batch_size != 0)
+
+    charlen = []
+    chars = []
+    char2id, id2char = lookup_dicts(special_chars)
+    for sentence in texts:
+        sentence = sentence.translate(str.maketrans('', '', string.punctuation))
+        char_converted = [char2id[char] if char != ' ' else char2id['<SPACE>'] for char in list(sentence)]
+        chars.append([char2id['<SOS>']] + char_converted + [char2id['<EOS>']])
+        charlen.append(len(chars[-1]))
+
+    return chars, charlen
+
+def lookup_dicts(special_chars):
+    """
+    Args:
+        special_chars: special charactors, <PAD>, <SOS>, <EOS>, <SPACE>
+    Returns:
+        char2id: dict, from character to index.
+        id2char: dict, from index to character.
+    """
+
+    alphas = list(string.ascii_uppercase[:26])
+    chars = special_chars + alphas
+    char2id = {}
+    id2char = {}
+    for i, c in enumerate(chars):
+        char2id[c] = i
+        id2char[i] = c
+    return char2id, id2char
+
+def batch_gen(feats, featlen, batch_size=5, shuffle=True):
+    """
+    Returns:
+        iter: Batch iterator.
+        batch_num: Number of batches.        
+    """
+
+    assert len(feats) == len(featlen)
+    num_batches = len(feats) // batch_size + int(len(feats) % batch_size != 0)
+
     def generator():
         buff = []
-        for i, x in enumerate(feats_list):
-            if i % batch_size == 0:
-                len_batch = length_list1[i:i+batch_size]
-                max_len = max(len_batch)
+        if shuffle:
+            rand_idx = np.random.permutation(len(feats))
+            feats_, featlen_  = feats[rand_idx], featlen[rand_idx]
+        else:
+            feats_, featlen_ = feats, featlen
             
+        for i, x in enumerate(feats_):
             if i % batch_size == 0 and buff:
                 yield np.stack(buff, 0), len_batch
                 buff = []
+            if i % batch_size == 0:
+                len_batch = featlen_[i:i+batch_size]
+                max_len = max(len_batch)
             padded = np.zeros([max_len - x.shape[0], x.shape[1]], dtype=np.float32)
             x_padded = np.concatenate([x, padded], 0)
             buff.append(x_padded)
@@ -134,7 +174,7 @@ def batch_gen(feats_list, length_list1, batch_size=5):
                                              output_types=types, 
                                              output_shapes=shapes)
     dataset.batch(batch_size).prefetch(1)
-    dataset = dataset.shuffle(128*batch_size)
+    dataset = dataset.shuffle(batch_size*64)
     dataset = dataset.repeat() 
     iter = dataset.make_initializable_iterator()
     return iter, num_batches
@@ -142,16 +182,17 @@ def batch_gen(feats_list, length_list1, batch_size=5):
 #def load_text(path):
 
 if __name__ == "__main__":
-    s = time.time()
     sess = tf.Session()
     libri_path = './data/LibriSpeech/dev-clean'
     texts, audio_path = data_preparation(libri_path)
-    X, X_len = load_audio(audio_path, sess, prepro_batch=64)
+    special_chars = ['<PAD>', '<SOS>', '<EOS>', '<SPACE>']
+    chars, charlen = process_texts(special_chars, texts)
+    X, X_len = process_audio(audio_path, sess, prepro_batch=64)
     iter_, num_batches = batch_gen(X, X_len, batch_size=32)
     x = iter_.get_next()
     sess.run(tf.global_variables_initializer())
     sess.run(iter_.initializer)
     print(num_batches)
-    for _ in range(100):
-        print(sess.run(x)[0].shape, max(sess.run(x)[1]), sess.run(x)[1].shape)
-    print(time.time()-s)
+    for _ in range(5):
+        d = sess.run(x)
+        print(d[0].shape, d[1], d[1].shape)
