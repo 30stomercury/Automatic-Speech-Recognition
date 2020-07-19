0597adcfbec92b0242e4ea8936d1dacef80a875d
30stomercury <ff936tw@gmail.com>
2020-07-19 21:44:53 +0800

    tokenizers

--- utils/tokenizer.py
+++ utils/tokenizer.py
@@ -1,6 +1,8 @@
 import string
 from tokenizers import CharBPETokenizer
 
+SPECIAL_TOKENS = ['<PAD>', '<SOS>', '<EOS>', '<SPACE>']
+
 def lookup_dicts(special_tokens):
     """
     Args:
@@ -30,7 +32,7 @@ def train_subword_tokenizer(size, special_tokens, path):
     """
     tokenizer = CharBPETokenizer()
     tokenizer.train(
-        [path+"/corpus.txt"],
+        [path+"/corpus_all.txt"],
         vocab_size=size,
         min_frequency=2,
         show_progress=True,
@@ -38,51 +40,24 @@ def train_subword_tokenizer(size, special_tokens, path):
     )
     tokenizer.save(path, "bpe")
 
-class text_encoder:
-    "char tokenizarion and subword tokenization" 
-    def __init__(self, unit, special_tokens):
-        """
-        Args:
-            unit: unit used for encoding strings, char or subword.
-            special_tokens: special charactors, <PAD>, <SOS>, <EOS>, <SPACE>
-        """
-        self.unit = unit
-        self.special_tokens = special_tokens
+class Subword_Encoder:
+    "Subword tokenization" 
 
-        # define char2id and id2char used in _encode_chars()
-        if self.unit == "char":
-            self.char2id, self.id2char = lookup_dicts(special_tokens)
-            self.encode = self._encode_chars
-            self.id_to_token = self.id2char
-            self.token_to_id = self.char2id
-
-        # utilize "tokenizers" library
-        elif self.unit == "subword":
-            self.subword_tokenizer = CharBPETokenizer(vocab_file="subword/bpe-vocab.json", merges_file="subword/bpe-merges.txt")
-            self.encode = self._encode_subwords
-            self.id_to_token = self._id_to_subword()
-            self.token_to_id = self._subword_to_id()
-        else:
-            raise Exception('Unit not support!') 
-
-    def get_vocab_size(self):
-        if self.unit == "char":
-            return len(self.id2char)
-        else:
-            return self.subword_tokenizer.get_vocab_size()   
-        
-    def _encode_chars(self, sentence, with_eos):
+    def __init__(self, paath='subword/'):
         """ 
         Args:
-            sentence: str, texts to be encoded.
-            with_eos: end with <EOS> token.
-        Returns:
-            tokens: list, encoded sequence.
+            path: str, a path to vocab file.
         """
-        tokens = [self.char2id[char] if char != ' ' else self.char2id['<SPACE>'] for char in list(sentence)]
-        if with_eos:
-            tokens += [self.char2id['<EOS>']]
-        return tokens
+        
+        # Load vocab
+        self.subword_tokenizer = CharBPETokenizer(vocab_file=path+"/bpe-vocab.json", merges_file=path+"/bpe-merges.txt")
+
+        self.encode = self._encode_subwords
+        self.id_to_token = self._id_to_subword()
+        self.token_to_id = self._subword_to_id()
+
+    def get_vocab_size(self):
+        return self.subword_tokenizer.get_vocab_size()   
 
     def _encode_subwords(self, sentence, with_eos):
         """ 
@@ -108,3 +83,35 @@ class text_encoder:
         for i in range(self.get_vocab_size()):
             subword2id[self.subword_tokenizer.id_to_token(i)] = i
         return subword2id
+
+class Char_Encoder:
+    "Char tokenizarion"
+
+    def __init__(self):
+        """
+        Args:
+            special_tokens: special charactors, <PAD>, <SOS>, <EOS>, <SPACE>
+        """
+
+        # define char2id and id2char used in _encode_chars()
+        self.char2id, self.id2char = lookup_dicts(SPECIAL_TOKENS)
+        self.encode = self._encode_chars
+        self.id_to_token = self.id2char
+        self.token_to_id = self.char2id
+
+
+    def get_vocab_size(self):
+        return len(self.id2char)
+        
+    def _encode_chars(self, sentence, with_eos):
+        """ 
+        Args:
+            sentence: str, texts to be encoded.
+            with_eos: end with <EOS> token.
+        Returns:
+            tokens: list, encoded sequence.
+        """
+        tokens = [self.char2id[char] if char != ' ' else self.char2id['<SPACE>'] for char in list(sentence)]
+        if with_eos:
+            tokens += [self.char2id['<EOS>']]
+        return tokens
