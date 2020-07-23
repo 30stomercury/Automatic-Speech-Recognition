e89abef8134e6edcec336d719dd5d58fb1b69c81
30stomercury <ff936tw@gmail.com>
2020-07-23 14:21:45 +0800

    clean codes

--- create_tfrecord.py
+++ create_tfrecord.py
@@ -6,6 +6,11 @@ import joblib
 from glob import glob
 from tqdm import tqdm
 
+
+# setup
+UNIT = 'subword'
+MAXLEN = 1710
+
 def load_feats(path, cat):
     partitions = np.sort(glob(path+"/"+cat+"_feats*"))
     feats = []
@@ -44,7 +49,7 @@ def create_tfrecords(X, y, filename, num_files=5):
         st = i * num_records_per_file                                              # start point (inclusive)
         ed = (i+1) * num_records_per_file if i != num_files-1 else len(tokens)     # end point (exclusive)
 
-        for feat, token in tqdm(zip(feats, tokens)):
+        for feat, token in tqdm(zip(feats[st : ed], tokens[st : ed])):
             
             # construct 'example' object containing 'feat', 'token' 
             example = tf.train.Example(features=tf.train.Features(feature={
@@ -61,19 +66,46 @@ def create_tfrecords(X, y, filename, num_files=5):
     print("Total records: {}".format(total_count))
 
 
+"""
 unit = 'subword'
-feat_dir = 'data/LibriSpeech-360/features_mfcc/'
-#train_feats_100 = joblib.load(feat_dir+"/train_feats.pkl")
-train_feats_100 = load_feats(feat_dir, "train")
-train_tokens_100 = np.load(
-    feat_dir+"/train_{}s.npy".format(unit), allow_pickle=True)
+feat_dir = 'data/LibriSpeech-100/features_mfcc/'
+train_feats = joblib.load(feat_dir+"/train_feats.pkl")
+train_tokens = np.load(
+    'data/LibriSpeech-100/features_mfcc_bpe_5k'+"/train_{}s.npy".format(unit), allow_pickle=True)
+train_tokenlen = np.load(
+    'data/LibriSpeech-100/features_mfcc_bpe_5k'+"/train_{}len.npy".format(unit), allow_pickle=True)
 train_featlen = np.load(
     feat_dir+"/train_featlen.npy", allow_pickle=True)
-train_tokens = np.load(
-    feat_dir+"/train_{}s.npy".format(unit), allow_pickle=True)
-train_tokenlen = np.load(
-    feat_dir+"/train_{}len.npy".format(unit), allow_pickle=True)
 
-X = train_feats_100
-y = train_tokens_100
-create_tfrecords(X, y, 'data/tfrecord/train-360', 10)
+#train_feats_100 = load_feats(feat_dir, "train")
+train_tokens_bpe_500 = np.load(
+    feat_dir+"/train_{}s.npy".format(unit), allow_pickle=True)
+
+train_tokenlen_bpe_500 = np.load(
+    'data/LibriSpeech-100/features_mfcc'+"/train_{}len.npy".format(unit), allow_pickle=True)
+
+print(sum(train_tokenlen_bpe_500), sum(train_tokenlen))
+
+print(sum(train_featlen > 1710))
+X = train_feats[train_featlen < 1710]
+y = train_tokens[train_featlen < 1710]
+print(len(X), len(y))
+create_tfrecords(X, y, 'data/tfrecord_bpe_5k/train-100', 1)
+"""
+
+# 100h, 360h, 500h
+feat_dir = 'data/LibriSpeech-{}/features_mfcc/'
+token_dir = 'data/LibriSpeech-{}/features_mfcc_bpe_5k/'
+for h in [100]:
+    train_feats = load_feats(feat_dir.format(h), "train")
+    train_featlen = np.load(
+            feat_dir.format(h)+"/train_featlen.npy", allow_pickle=True)
+    train_tokens = np.load(
+            token_dir.format(h)+"/train_{}s.npy".format(UNIT), allow_pickle=True)
+
+    assert len(train_feats) == len(train_tokens)
+
+    X = train_feats[train_featlen < MAXLEN]
+    y = train_tokens[train_featlen < MAXLEN]
+
+    create_tfrecords(X, y, 'data/tfrecord_bpe_5k/train-{}'.format(h), h // 100)
