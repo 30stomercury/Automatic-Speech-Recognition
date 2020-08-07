a018c8e348219e6276600a85ee624d9d4db2a1d4
30stomercury <ff936tw@gmail.com>
2020-08-07 18:29:36 +0800

    add attention weights

--- las/beam_search.py
+++ las/beam_search.py
@@ -1,25 +1,31 @@
 import numpy as np
 import tensorflow as tf
 
+NORM=True
+
+
 class BeamState(object):
 
-    def __init__(self, token_ids, log_prob, dec_state, lm_state):
+    def __init__(self, token_ids, log_prob, att, dec_state, lm_state):
         """hypothesis
         Args:
-            token_id: Index of the starting tokenactor, 1 corresponding to <SOS>.       
-            log_prob: Log probability of the starting tokenactor.
-            state: Decoder initial state. Zero state of RNN cell.
+            token_id: List, Index of the starting tokenactor, 1 corresponding to <SOS>.       
+            log_prob: Float, Log probability of the starting tokenactor.
+            att:      List, attention weights.
+            state:    Tuple, Decoder initial state. Zero state of RNN cell.
         """
         self.token_ids = token_ids
         self.log_prob = log_prob
+        self.att = att
         self.dec_state = dec_state
         self.lm_state = lm_state
 
-    def update(self, token_id, log_prob, dec_state, lm_state):
+    def update(self, token_id, log_prob, att, dec_state, lm_state):
         """Return new beam state based on last decoding results."""
         return BeamState(
-                    self.token_ids+[token_id], 
+                    self.token_ids+[token_id],
                     self.log_prob+log_prob, 
+                    self.att + [att],
                     dec_state, 
                     lm_state)
     
@@ -30,8 +36,8 @@ class BeamSearch(object):
         Args:
             args: arguments.
             las: LAS.
-            token_to_id: dict, map from tokenactor to index.
-            language_model: RNNLM class.
+            token_to_id: dict, map from token to index.
+            language_model: class, RNNLM.
         """    
         self.args = args
         self.listener = las.listener
@@ -41,18 +47,18 @@ class BeamSearch(object):
         self.start_id = token_to_id['<SOS>']
         self.end_id = token_to_id['<EOS>']
 
-        if args.unit != "char" and args.unit != "subword":
+        if args.unit.lower() != "char" and args.unit.lower() != "subword":
           raise ValueError('Other units are currently not support!')
 
         # build graph
-        self._build_encode()
+        self._build_encode(self.args.enc_type)
         self._build_decode()
         
         # Language Model
         if args.apply_lm:
             self.lm = language_model
 
-    def decode(self, sess, xs):
+    def decode(self, sess, xs): # TODO rewrite decode -> clean codes
         """
         sess: tf session
         xs: a tuple of 
@@ -60,6 +66,7 @@ class BeamSearch(object):
             - audiolen: (1,), original feature length.
         """
         audio, audiolen = xs
+
         # beam search can only perform on single utterance.
         if len(audio) != 1:
           raise ValueError('batch size must be 1 while performing beam search.')
@@ -77,10 +84,18 @@ class BeamSearch(object):
         if self.args.apply_lm:
             lm_init_state = self._get_lm_init(sess)
 
-        beam_set = [BeamState([self.start_id], 0, dec_init_state, lm_init_state)]*self.beam_size 
+        beam_set = [BeamState(token_ids=[self.start_id], 
+                              log_prob=0, 
+                              att=[np.zeros(h.shape[1])], 
+                              dec_state=dec_init_state, 
+                              lm_state=lm_init_state)] * self.beam_size 
         selected_beam_state = []
+
         while t < dec_step and len(selected_beam_state) < self.beam_size:
             prev_token_ids = [b.token_ids[-1] for b in beam_set]
+            prev_align = [b.att[-1] for b in beam_set]
+
+            ## used to collect beams
             beam_set_bank = []
 
             # put N nodes into a batch and feed it to the model
@@ -88,12 +103,13 @@ class BeamSearch(object):
             prev_dec_states = np.concatenate(prev_dec_states, 1)
 
             # decoding
-            logits, dec_states = self._get_decode(
-                                    sess, h, enc_len, prev_token_ids, prev_dec_states)
-            if self.args.apply_lm:
+            logits, dec_states, alphas = self._get_decode(
+                                    sess, h, enc_len, prev_token_ids, prev_align, prev_dec_states)
+
+            if self.args.apply_lm: 
                 prev_lm_states = [b.lm_state for b in beam_set]
                 prev_lm_states = np.split(
-                                np.concatenate(prev_lm_states, 2).reshape([-1, 512]), 4)
+                                np.concatenate(prev_lm_states, 2).reshape([-1, 512]), 4) # TODO move to args
                 # resoring w/ language model
                 lm_out, lm_states = self._get_lm(
                                         sess, prev_token_ids, prev_lm_states)
@@ -104,27 +120,31 @@ class BeamSearch(object):
 
             # collect nodes
             for i in range(num_beam):
-                topk_ids = np.argsort(logits[i])[4:]       # => argsort is in acending order
+                topk_ids = np.argsort(logits[i])[64:]       # => argsort is in acending order
                 topk_probs = logits[i][topk_ids]
-                for j in range(len(topk_ids)):  # => search each node
-                    if t > 0 and topk_ids[j] == self.token_to_id["<SOS>"]:
+
+                for j in range(len(topk_ids)):              # => search each node
+                    if t > 0 and topk_ids[j] == self.start_id:
                         continue
 
                     if self.args.apply_lm:
-                        beam_set_bank.append(beam_set[i].update(
-                                            topk_ids[j], topk_probs[j], 
-                                            (dec_states[0][i], dec_states[1][i]),
-                                             lm_states[:, :, i, :]))
+                        beam_set_bank.append(beam_set[i].update(topk_ids[j], 
+                                                        topk_probs[j], 
+                                                        alphas[i]
+                                                        (dec_states[0][i], dec_states[1][i]),
+                                                        lm_states[:, :, i, :]))
                     else:
-                        beam_set_bank.append(beam_set[i].update(
-                                            topk_ids[j], topk_probs[j], 
-                                            (dec_states[0][i], dec_states[1][i]),
-                                             None))
+                        beam_set_bank.append(beam_set[i].update(topk_ids[j], 
+                                                        topk_probs[j], 
+                                                        alphas[i],
+                                                        (dec_states[0][i], dec_states[1][i]),
+                                                        None))
 
+            # used to collect topk beams
             beam_set = []                 
-            norm = True 
+
             # sort by log prob
-            topk_beam_state = self._select_best_k(beam_set_bank, norm)
+            topk_beam_state = self._select_best_k(beam_set_bank, NORM)
             for b in topk_beam_state:
                 if b.token_ids[-1] == self.end_id:
                     selected_beam_state.append(b)
@@ -137,36 +157,48 @@ class BeamSearch(object):
 
         return self._select_best_k(selected_beam_state, norm)
 
-    def _build_encode(self):
+    def _build_encode(self, enc_type):
         """build encoder graph"""
         # for encoder
         self.audio = tf.placeholder(tf.float32,
-                                     shape=[None, None, self.args.feat_dim], name='audio')
+                                     shape=[None, None, self.args.feat_dim, 3], name='audio')
         self.audiolen = tf.placeholder(tf.int32, shape=[None], name='audiolen')
+
         # build graph
         self.enc_out, enc_state, self.enc_len = \
-                        self.listener(self.audio, self.audiolen, is_training=False)
+                        self.listener(self.audio, self.audiolen, encoder=enc_type, is_training=False)
 
     def _build_decode(self):
         """build decoder graph"""
         # for decoder
         self.h = tf.placeholder(tf.float32,
-                                shape=[None, None, self.args.enc_units*2], name='enc_out')
+                                shape=[None, None, self.args.enc_units], 
+                                name='enc_out')
         self.h_len = tf.placeholder(tf.int32, shape=[None], name='enc_len')
         self.prev_token_id = tf.placeholder(tf.int32, shape=[None], name='token_id')
+        self.prev_align = tf.placeholder(tf.float32,
+                                shape=[None, None])
         self.dec_state_packed = tf.placeholder(tf.float32, 
-                                [self.args.num_dec_layers, None, self.args.dec_units], name='rnn_state')
+                                [self.args.num_dec_layers, None, self.args.dec_units], 
+                                name='rnn_state')
+
         # form to tuple state
         l = tf.unstack(self.dec_state_packed, axis=0)
         rnn_tuple_state = tuple(
                 [l[i]
                  for i in range(self.args.num_dec_layers)])
+
         # look up
         prev_token = self.speller._look_up(self.prev_token_id)
         prev_token = tf.reshape(prev_token, [-1, self.args.embedding_size])
+
         # build graph, specify the variable scope
-        self.cur_token, self.dec_state, self.alphas = self.speller.decode(
-                            self.h, self.h_len, rnn_tuple_state, prev_token, is_training=False)
+        self.cur_token, self.dec_state, self.alphas = self.speller.decode(self.h, 
+                                                            self.h_len, 
+                                                            rnn_tuple_state, 
+                                                            prev_token, 
+                                                            self.prev_align, 
+                                                            is_training=False)
 
     def _get_encode(self, sess, audio, audiolen):
         """get encoder output"""
@@ -176,18 +208,20 @@ class BeamSearch(object):
                     }
         return sess.run([self.enc_out, self.enc_len], feed_dict)
 
-    def _get_decode(self, sess, enc_out, enc_len, prev_token_id, dec_state_packed):
+    def _get_decode(
+            self, sess, enc_out, enc_len, prev_token_id, prev_align, dec_state_packed):
         """get decoder output"""
         N = len(prev_token_id)
         feed_dict = {
                     self.h: np.tile(enc_out, (N, 1, 1)),
                     self.h_len: np.tile(enc_len, N),
                     self.prev_token_id: prev_token_id,
+                    self.prev_align: prev_align,
                     self.dec_state_packed: dec_state_packed
                     }
         logits, states, alphas = sess.run([self.cur_token, self.dec_state, self.alphas], feed_dict)
 
-        return logits, states
+        return logits, states, alphas
     
     def _get_lm(self, sess, prev_token_id, lm_state_packed):
         """get decoder output"""
@@ -219,10 +253,11 @@ class BeamSearch(object):
         """Get variables for decode graph."""
         # create restore dict for decode scope
         var = {}
-        var_all = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, '')
+        var_all = tf.global_variables()
         var_att = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'decode/attention/Variable')
         var_decode = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'decode/')
         var_list = [i[0] for i in tf.train.list_variables(save_path)]
+
         for v in var_all:
             name = v.name.split(":")[0]
             if v in var_att:
@@ -231,14 +266,15 @@ class BeamSearch(object):
                 var['Speller/' + name] = v
             elif name in var_list and name != "global_step":
                 var[v.name.split(":")[0]] = v
+
         return var
 
     def restore_las(self, sess, save_path, restore_epoch):
         """Restore LAS ckpt."""
         ckpt = tf.train.latest_checkpoint(save_path)
         if restore_epoch != -1:
-            ckpt = args.save_path+"/las_E{}".format(restore_epoch)
-        var_las = self._get_decode_varlist(ckpt) # TODO
+            ckpt = save_path+"/las_E{}".format(restore_epoch)
+        var_las = self._get_decode_varlist(ckpt) 
         # restore
         saver = tf.train.Saver(var_list=var_las)
         saver.restore(sess, ckpt)
